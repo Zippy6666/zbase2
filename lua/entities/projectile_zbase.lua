72a614adb898eb3c013fdeb8bfb736a95c9f057b
ZIPPY <simperzippy@gmail.com>
2023-11-09 15:57:33 +0100

    projectile base

---
+++ lua/entities/projectile_zbase.lua
@@ -0,0 +1,11 @@
+AddCSLuaFile()
+------------------------------------------------------------------------------------=#
+ENT.Base = "base_gmodentity"
+ENT.Type = "anim"
+ENT.Author = "Zippy"
+ENT.PrintName = "Projectile"
+ENT.Spawnable = true
+------------------------------------------------------------------------------------=#
+
+
+ENT.Model = "models/dav0r/hoverball.mdl"
