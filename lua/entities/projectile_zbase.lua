e072d5b0464ada14c77c6192e2a08facdb6128bf
ZIPPY <simperzippy@gmail.com>
2023-11-10 13:24:17 +0100

    more projectile stuff

--- lua/entities/projectile_zbase.lua
+++ lua/entities/projectile_zbase.lua
@@ -1,20 +1,33 @@
 AddCSLuaFile()
-------------------------------------------------------------------------------------=#
+
+
 ENT.Base = "base_gmodentity"
 ENT.Type = "anim"
 ENT.Author = "Zippy"
 ENT.PrintName = "Projectile"
-ENT.Spawnable = true
+ENT.Spawnable = false
 ENT.Category = "ZBase"
 ENT.IsZBaseProjectile = true
-------------------------------------------------------------------------------------=#
 
-ENT.Model = "models/dav0r/hoverball.mdl" -- Model to use
+
+ENT.Model = "models/spitball_medium.mdl" -- Model to use
 ENT.Invisible = false -- Should the model be invisible?
+
+
+ENT.StartHealth = false -- Health of the projectile, set to false to disable projectile health
+
+
 ENT.Gravity = false -- Should the projectile have gravity?
-ENT.Damage_Disorient = true -- Should the projectile blast damage function disorient players (be deafening)?
-ENT.GravityGun_Pickup = true -- Can the projectile be picked up by the gravity gun?
-ENT.GravityGun_Punt = true -- Can the projectile be punted by the gravity gun?
+ENT.GravityGun_Pickup = false -- Can the projectile be picked up by the gravity gun?
+ENT.GravityGun_Punt = false -- Can the projectile be punted by the gravity gun?
+
+
+ENT.OnHitDamage = 10 -- Projectile damage on hit, set to false to disable
+ENT.OnHitDamageType = DMG_GENERIC -- Projectile damage type on hit
+
+
+ENT.Damage_Disorient = false -- Should any damage (direct or radius) from the projectile disorient players (be deafening)?
+
 
 
 
@@ -24,27 +37,28 @@ ENT.GravityGun_Punt = true -- Can the projectile be punted by the gravity gun?
 
     -- When the projectile is created
 function ENT:PostInit()
-    if SERVER then
-        -- Dynamic light
-        local glow = ents.Create("light_dynamic")
-        glow:SetKeyValue("brightness", "2")
-        glow:SetKeyValue("distance", "200")
-        glow:SetPos(self:WorldSpaceCenter())
-        glow:SetParent(self)
-        glow:Fire("Color", "50 50 255")
-        glow:Spawn()
-        glow:Activate()
-        glow:Fire("TurnOn", "", 0)
-        self:DeleteOnRemove(glow)
+    -- Example:
 
-        -- Particle
-        ParticleEffectAttach("larvae_glow", PATTACH_ABSORIGIN_FOLLOW, self, 0)
+    -- if SERVER then
+    --     -- Dynamic light
+    --     local glow = ents.Create("light_dynamic")
+    --     glow:SetKeyValue("brightness", "2")
+    --     glow:SetKeyValue("distance", "200")
+    --     glow:SetPos(self:WorldSpaceCenter())
+    --     glow:SetParent(self)
+    --     glow:Fire("Color", "50 255 155")
+    --     glow:Spawn()
+    --     glow:Activate()
+    --     glow:Fire("TurnOn", "", 0)
+    --     self:DeleteOnRemove(glow)
 
-        -- Looping sound
-        local snd = CreateSound( self, "Weapon_Gauss.ChargeLoop" )
-        snd:Play()
-        self:CallOnRemove("StopLoopSound", function() snd:Stop() end)
-    end
+    --     -- Particle
+    --     ParticleEffectAttach("vortigaunt_hand_glow", PATTACH_ABSORIGIN_FOLLOW, self, 0)
+
+    --     -- Looping sound
+    --     self.HumSound = CreateSound( self, "Weapon_Gauss.ChargeLoop" )
+    --     self.HumSound:Play()
+    -- end
 end
 ------------------------------------------------------------------------------------=#
 
@@ -56,55 +70,70 @@ end
 
     -- When the projectile hits an entity
 function ENT:OnHit( ent, data )
-    -- Direct damage --
-    -- ProjectileDamage( entity, damage, damageType )
-    -- VVV Example VVV
-    self:ProjectileDamage( ent, 10, DMG_SONIC )
-    ------------------------------------------------------=#
+    -- Example:
 
-
-    -- Blast/Radius damage --
-    -- ProjectileBlastDamage( damage, damageType, radius, force )
-    -- VVV Example VVV
-    self:ProjectileBlastDamage( 10, DMG_DISSOLVE, 400, 50 )
-    ------------------------------------------------------=#
-
-
-
-    -- Sound
-    self:EmitSound("Weapon_Mortar.Impact")
-
-    -- Screen shake
-    util.ScreenShake(self:WorldSpaceCenter(), 5, 200, 1, 400)
-
-    -- Dynamic light flash
-    local glow = ents.Create("light_dynamic")
-    glow:SetKeyValue("brightness", "5")
-    glow:SetKeyValue("distance", "500")
-    glow:SetPos(self:WorldSpaceCenter())
-    glow:Fire("Color", "50 50 255")
-    glow:Spawn()
-    glow:Activate()
-    glow:Fire("TurnOn", "", 0)
-    SafeRemoveEntityDelayed(glow, 0.15)
-
-    -- Explosion particle
-    ParticleEffect("Weapon_Combine_Ion_Cannon_Explosion", self:WorldSpaceCenter(), AngleRand())
-
-
-
-    self:SetMoveType(MOVETYPE_NONE) -- Prevents stupid physics sounds from playing when the projectile hits something
-    self:Remove() -- Lastly, remove the projectile
+    self:Die()
 end
 ------------------------------------------------------------------------------------=#
 
     -- Called continiously
 function ENT:OnThink()
+    -- Example:
+
+    -- if SERVER then
+    --     -- Random pitches for the looping sound
+    --     self.HumSound:ChangePitch(math.random(60, 140), 1)
+    -- end
 end
 ------------------------------------------------------------------------------------=#
 
+    -- When the projectile takes damage
+function ENT:CustomOnTakeDamage(dmginfo)
+end
+------------------------------------------------------------------------------------=#
 
+    -- When the projectile "dies"
+function ENT:OnKill(dmginfo)
+    -- Example:
 
+    -- Blast/Radius damage --
+    -- ProjectileBlastDamage( damage, damageType, radius, force )
+    -- VVV Example VVV
+    -- self:ProjectileBlastDamage( 10, DMG_GENERIC, 400, 50 )
+    ------------------------------------------------------=#
+
+    -- Sound
+    -- self:EmitSound("Weapon_Mortar.Impact")
+
+    -- Screen shake
+    -- util.ScreenShake(self:WorldSpaceCenter(), 5, 200, 1, 400)
+
+    -- Dynamic light flash
+    -- local glow = ents.Create("light_dynamic")
+    -- glow:SetKeyValue("brightness", "5")
+    -- glow:SetKeyValue("distance", "500")
+    -- glow:SetPos(self:WorldSpaceCenter())
+    -- glow:Fire("Color", "50 50 255")
+    -- glow:Spawn()
+    -- glow:Activate()
+    -- glow:Fire("TurnOn", "", 0)
+    -- SafeRemoveEntityDelayed(glow, 0.15)
+
+    -- Explosion particle
+    -- ParticleEffect("Weapon_Combine_Ion_Cannon_Explosion", self:WorldSpaceCenter(), AngleRand())
+end
+------------------------------------------------------------------------------------=#
+
+    -- When the projectile is removed
+function ENT:CustomOnRemove()
+    -- Example:
+
+    -- if SERVER then
+    --     -- Stop the looping sound
+    --     self.HumSound:Stop()
+    -- end
+end
+------------------------------------------------------------------------------------=#
 
 
 
@@ -129,7 +158,18 @@ function ENT:Initialize()
 
             self:PhysInit(phys)
         end
+
+
+        if self.StartHealth then
+            self:SetHealth(self.StartHealth)
+            self:SetMaxHealth(self.StartHealth)
+        end
+
+        self:SetCollisionGroup(COLLISION_GROUP_PROJECTILE)
+        self:SetMoveCollide(MOVECOLLIDE_FLY_BOUNCE)
     end
+
+    self.GravGunPlayer = NULL
 
     self:PostInit()
 end
@@ -140,6 +180,14 @@ end
 ------------------------------------------------------------------------------------=#
 function ENT:PhysicsCollide( colData, collider )
     self:OnHit( colData.HitEntity, colData )
+
+    -- if util.GetSurfaceData( colData.TheirSurfaceProps ).material==MAT_GRATE then
+
+    -- end
+
+    if self.OnHitDamage then
+        self:ProjectileDamage(colData.HitEntity, self.OnHitDamage, self.OnHitDamageType)
+    end
 end
 ------------------------------------------------------------------------------------=#
 function ENT:ProjectileDamage( ent, dmg, dmgtype )
@@ -149,7 +197,11 @@ function ENT:ProjectileDamage( ent, dmg, dmgtype )
     local dmginfo = DamageInfo()
 
     dmginfo:SetInflictor(self)
-    dmginfo:SetAttacker(IsValid(own) && own or self)
+    dmginfo:SetAttacker(
+        (IsValid(self.GravGunPlayer) && self.GravGunPlayer)
+        or (IsValid(own) && own)
+        or self
+    )
     dmginfo:SetDamage(dmg)
     dmginfo:SetDamageType(dmgtype)
     dmginfo:SetDamageForce(self:GetVelocity()*10)
@@ -166,7 +218,11 @@ function ENT:ProjectileBlastDamage( dmg, dmgtype, radius, force )
     local dmginfo = DamageInfo()
 
     dmginfo:SetInflictor(self)
-    dmginfo:SetAttacker(IsValid(own) && own or self)
+    dmginfo:SetAttacker(
+        (IsValid(self.GravGunPlayer) && self.GravGunPlayer)
+        or (IsValid(own) && own)
+        or self
+    )
     dmginfo:SetDamage(dmg)
     dmginfo:SetDamageType(dmgtype)
 
@@ -184,6 +240,32 @@ function ENT:ProjectileBlastDamage( dmg, dmgtype, radius, force )
     util.BlastDamageInfo(dmginfo, self:WorldSpaceCenter(), radius)
 end
 ------------------------------------------------------------------------------------=#
+function ENT:Die( dmg )
+    if self.Dead then return end
+    self.Dead = true
+    self:OnKill( dmg )
+    self:SetMoveType(MOVETYPE_NONE)
+    self:Remove()
+end
+------------------------------------------------------------------------------------=#
+function ENT:OnTakeDamage( dmg )
+    if !self.StartHealth then return end
+
+    self:CustomOnTakeDamage()
+
+    self:SetHealth(self:Health()-dmg:GetDamage())
+
+    if self:Health() <= 0 then
+        self:Die( dmg )
+    end
+
+    return dmg:GetDamage()
+end
+------------------------------------------------------------------------------------=#
+function ENT:OnRemove()
+    self:CustomOnRemove()
+end
+------------------------------------------------------------------------------------=#
 hook.Add("PostEntityTakeDamage", "ZBaseProjectile", function( ent, dmg )
     local infl = dmg:GetInflictor()
 
@@ -193,8 +275,18 @@ hook.Add("PostEntityTakeDamage", "ZBaseProjectile", function( ent, dmg )
 end)
 ------------------------------------------------------------------------------------=#
 hook.Add("GravGunPunt", "ZBaseProjectile", function( ply, ent )
-    if ent.IsZBaseProjectile && !ent.GravityGun_Punt then
-        return false
+    if ent.IsZBaseProjectile then
+        if ent.GravityGun_Punt then
+            ent.GravGunPlayer = ply
+            ent:SetOwner(NULL)
+
+            timer.Create("ResetGravGunPlayer", 5, 1, function()
+                if !IsValid(ent) then return end
+                ent.GravGunPlayer = NULL
+            end)
+        else
+            return false
+        end
     end
 end)
 ------------------------------------------------------------------------------------=#
