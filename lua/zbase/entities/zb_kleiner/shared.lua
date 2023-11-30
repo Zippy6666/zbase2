362f1f514eeec541d31a386dd371623619872059
ZIPPY <simperzippy@gmail.com>
2023-11-30 15:14:35 +0100

    more npcssssssss

---
+++ lua/zbase/entities/zb_kleiner/shared.lua
@@ -0,0 +1,14 @@
+local NPC = FindZBaseTable(debug.getinfo(1,'S'))
+
+
+-- The NPC class
+-- Can be any existing NPC in the game
+-- If you want to make a human that can use weapons, you should probably use "npc_combine_s" or "npc_citizen" for example
+-- Use "npc_zbase_snpc" if you want to create a brand new SNPC
+NPC.Class = "npc_kleiner"
+
+
+NPC.Name = "Dr. Isaac Kleiner" -- Name of your NPC
+NPC.Category = "Humans + Resistance" -- Category in the ZBase tab
+NPC.Weapons = {"weapon_shotgun"} -- Example: {"weapon_rpg", "weapon_crowbar", "weapon_crossbow"}
+NPC.Inherit = "npc_zbase" -- Inherit features from any existing zbase npc
