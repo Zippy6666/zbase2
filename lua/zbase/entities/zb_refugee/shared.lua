b7e7fafee6ea28b5ab0ab0dc3c193ada1cd987bf
ZIPPY <simperzippy@gmail.com>
2023-12-01 11:40:43 +0100

    kleiner epic npc

--- lua/zbase/entities/zb_refugee/shared.lua
+++ lua/zbase/entities/zb_refugee/shared.lua
@@ -5,10 +5,10 @@ local NPC = FindZBaseTable(debug.getinfo(1,'S'))
 -- Can be any existing NPC in the game
 -- If you want to make a human that can use weapons, you should probably use "npc_combine_s" or "npc_citizen" for example
 -- Use "npc_zbase_snpc" if you want to create a brand new SNPC
-NPC.Class = "npc_turret_ceiling"
+NPC.Class = "npc_citizen"
 
 
-NPC.Name = "Ceiling Turret" -- Name of your NPC
-NPC.Category = "Combine" -- Category in the ZBase tab
+NPC.Name = "Refugee" -- Name of your NPC
+NPC.Category = "Humans + Resistance" -- Category in the ZBase tab
 NPC.Weapons = {} -- Example: {"weapon_rpg", "weapon_crowbar", "weapon_crossbow"}
-NPC.Inherit = "npc_zbase" -- Inherit features from any existing zbase npc
+NPC.Inherit = "zb_citizen" -- Inherit features from any existing zbase npc
--- lua/zbase/entities/zb_stalker/init.lua
+++ lua/zbase/entities/zb_stalker/init.lua
@@ -1,8 +1,27 @@
 local NPC = FindZBaseTable(debug.getinfo(1,'S'))
 
 
+NPC.StartHealth = 70 -- Max health
+
+
+-- ZBase faction
+-- Can be any string, all ZBase NPCs with the same faction will be allied
+-- Default factions:
+-- "combine" || "ally" || "zombie" || "antlion" || "none" || "neutral"
+    -- "none" = not allied with anybody
+    -- "neutral" = allied with everybody
+NPC.ZBaseStartFaction = "combine"
+
+
+-- Stalker internal variables
+NPC.m_iPlayerAggression = 1
+NPC.m_eBeamPower = 2
+
+
 --]]==============================================================================================]]
 function NPC:CustomInitialize()
-
+end
+--]]==============================================================================================]]
+function NPC:CustomThink()
 end
 --]]==============================================================================================]]
