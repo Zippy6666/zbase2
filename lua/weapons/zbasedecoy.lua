f7d9b11d86d14fdfa04225cce4ef1c100b13fd5c
ZIPPY <simperzippy@gmail.com>
2023-12-23 16:52:59 +0100

    i don even kno bruh

--- lua/weapons/zbasedecoy.lua
+++ lua/weapons/zbasedecoy.lua
@@ -17,11 +17,38 @@ SWEP.IsZBaseDecoyWep = true
 
 
 function SWEP:Initialize()
+
     self.ModelsPrecached = {}
+
+
+    hook.Add("Think", self, function()
+        self:Think2()
+    end)
+
 end
 
 
-function SWEP:Think()
+-- The sequal to think is finally here
+function SWEP:Think2()
+
+    local own = self:GetOwner()
+    if !IsValid(own) then return end
+
+
     self.WorldModel = Model( self:GetNWString("WorldModel") )
-    print(self.WorldModel)
+
+
+    if CLIENT then
+        -- local VMatrix = self:GetBoneMatrix(  )
+        -- local pos, ang = VMatrix:GetTranslation(), VMatrix:GetAngles()
+        -- self:SetPos(pos)
+    end
+    --self:FollowBone(own, )
+
+
+end
+
+
+function SWEP:OnRemove()
+    hook.Remove("Think", self)
 end
