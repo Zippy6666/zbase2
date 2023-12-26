b5297c65157d27ee2b857cabc8261e38c59b9a04
ZIPPY <simperzippy@gmail.com>
2023-12-26 14:24:25 +0100

    dohgsfdgfd

--- lua/zbase/sv_enginewep_translation.lua
+++ lua/zbase/sv_enginewep_translation.lua
@@ -9,8 +9,7 @@ ZBase_EngineWeapon_Attributes = {
 
 
     ["weapon_pistol"] = {
-        PrimaryShootSound = "Weapon_Pistol.NPC_Single",
-        PrimarySpread = 0.01, 
+        PrimaryShootSound = "Weapon_Pistol.NPC_Single", 
         PrimaryDamage = 3,
         Primary = {
             DefaultClip = 18, 
@@ -24,7 +23,7 @@ ZBase_EngineWeapon_Attributes = {
         NPCFireRate = 0.2, 
         NPCFireRestTimeMin = 0.2, 
         NPCFireRestTimeMax = 1,
-        NPCBulletSpreadMult = 1, 
+        NPCBulletSpreadMult = 1,
         NPCReloadSound = "Weapon_Pistol.Reload", 
         NPCShootDistanceMult = 0.75,
         NPCHoldType =  "pistol" -- https://wiki.facepunch.com/gmod/Hold_Types
@@ -40,7 +39,6 @@ ZBase_EngineWeapon_Attributes = {
 
     ["weapon_smg1"] = {
         PrimaryShootSound = "Weapon_SMG1.Single",
-        PrimarySpread = 0.02, 
         PrimaryDamage = 2,
         Primary = {
             DefaultClip = 45, 
@@ -54,7 +52,7 @@ ZBase_EngineWeapon_Attributes = {
         NPCFireRate = 0.1, 
         NPCFireRestTimeMin = 0.1, 
         NPCFireRestTimeMax = 0.1,
-        NPCBulletSpreadMult = 1.25, 
+        NPCBulletSpreadMult = 1, 
         NPCReloadSound = "Weapon_SMG1.NPC_Reload", 
         NPCShootDistanceMult = 0.75,
         NPCHoldType =  "smg" -- https://wiki.facepunch.com/gmod/Hold_Types
@@ -66,24 +64,26 @@ ZBase_EngineWeapon_Attributes = {
                                             AR2
     ======================================================================================================================================================
     --]]
-
+    
 
     ["weapon_ar2"] = {
         PrimaryShootSound = "Weapon_AR2.NPC_Single",
-        PrimarySpread = 0.01, 
         PrimaryDamage = 3,
         Primary = {
             DefaultClip = 30, 
             Ammo = "AR2", -- https://wiki.facepunch.com/gmod/Default_Ammo_Types
             ShellEject = false, 
             NumShots = 1,
+            MuzzleFlashFlags = 5,
+            TracerName = "AR2Tracer",
+            TracerChance = 1,
         },
         NPCBurstMin = 3,
         NPCBurstMax = 8,
         NPCFireRate = 0.1,
         NPCFireRestTimeMin = 0.2, 
         NPCFireRestTimeMax = 1,
-        NPCBulletSpreadMult = 1, 
+        NPCBulletSpreadMult = 0.25, 
         NPCReloadSound = "Weapon_AR2.NPC_Reload", 
         NPCShootDistanceMult = 1,
         NPCHoldType =  "ar2" -- https://wiki.facepunch.com/gmod/Hold_Types
@@ -109,7 +109,7 @@ ZBase_EngineWeapon_Attributes = {
             NumShots = 7,
         },
         NPCBurstMin = 1,
-        NPCBurstMax = 2,
+        NPCBurstMax = 1,
         NPCFireRate = 0,
         NPCFireRestTimeMin = 1, 
         NPCFireRestTimeMax = 2,
@@ -117,5 +117,35 @@ ZBase_EngineWeapon_Attributes = {
         NPCReloadSound = "Weapon_Shotgun.NPC_Reload", 
         NPCShootDistanceMult = 0.5,
         NPCHoldType =  "shotgun" -- https://wiki.facepunch.com/gmod/Hold_Types
+    },
+
+
+    --[[
+    ======================================================================================================================================================
+                                            AR2
+    ======================================================================================================================================================
+    --]]
+    
+
+    ["weapon_crossbow"] = {
+        PrimaryShootSound = "Weapon_AR2.NPC_Single",
+        PrimaryDamage = 10,
+        Primary = {
+            DefaultClip = 1, 
+            Ammo = "XBowBolt", -- https://wiki.facepunch.com/gmod/Default_Ammo_Types
+            ShellEject = false, 
+            NumShots = 1,
+            TracerChance = 0,
+            MuzzleFlash = false
+        },
+        NPCBurstMin = 3,
+        NPCBurstMax = 8,
+        NPCFireRate = 0.1,
+        NPCFireRestTimeMin = 0.2, 
+        NPCFireRestTimeMax = 1,
+        NPCBulletSpreadMult = 0.25, 
+        NPCReloadSound = "Weapon_AR2.NPC_Reload", 
+        NPCShootDistanceMult = 1,
+        NPCHoldType =  "ar2" -- https://wiki.facepunch.com/gmod/Hold_Types
     },
 }
