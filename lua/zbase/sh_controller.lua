bcbe5abd4d9caf4582df7c9f7f5cb9f8b780f991
ZIPPY <simperzippy@gmail.com>
2024-02-25 15:44:58 +0100

    controller?????

---
+++ lua/zbase/sh_controller.lua
@@ -0,0 +1,78 @@
+
+// Control ZBase NPCs
+
+ZBCtrlSys = {}
+
+
+if CLIENT then
+
+
+end
+
+
+
+if SERVER then
+    function ZBCtrlSys:StartControlling( ply, npc )
+
+        npc.IsZBPlyControlled = true
+
+        npc.ZBViewEnt = ents.Create("base_gmodentity")
+        npc.ZBViewEnt:SetPos( npc:GetPos() + (npc:GetUp()*npc:OBBMaxs().z*1.25) - (npc:GetForward()*npc:OBBMaxs().x*4) )
+        npc.ZBViewEnt:SetAngles(npc:GetAngles())
+        npc.ZBViewEnt:SetParent(npc)
+        npc.ZBViewEnt:SetNoDraw(true)
+        npc.ZBViewEnt:Spawn()
+        npc:DeleteOnRemove(npc.ZBViewEnt)
+        npc:SetSaveValue( "m_flFieldOfView", 1 ) -- No FOV, cant see shid
+        npc:CallOnRemove("StopControllingZB", function()
+            self:StopControlling(ply, npc)
+        end)
+
+
+        ply:SetViewEntity( npc.ZBViewEnt )
+
+
+        self:UpdateRelationShips()
+
+    end
+
+
+    function ZBCtrlSys:StopControlling( ply, npc )
+
+        if IsValid(npc) then
+            npc:SetSaveValue( "m_flFieldOfView", npc.FieldOfView )
+            npc.IsZBPlyControlled  = false
+
+            if IsValid(npc.ZBViewEnt) then
+                npc.ZBViewEnt:Remove()
+            end
+
+        end
+
+
+        if IsValid(ply) then
+            ply:SetViewEntity(ply)
+        end
+
+        
+        self:UpdateRelationShips()
+
+    end
+
+
+    function ZBCtrlSys:UpdateRelationShips()
+        for _, v in ipairs(ZBaseRelationshipEnts) do
+            v:ZBaseUpdateRelationships()
+        end
+    end
+
+
+    concommand.Add("ZBaseControlTest", function()
+        local ply = Entity(1)
+        local npc = ply:GetEyeTrace().Entity
+
+        if IsValid(npc) && npc.IsZBaseNPC then
+            ZBCtrlSys:StartControlling( ply, npc )
+        end
+    end)
+end
