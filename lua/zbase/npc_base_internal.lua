a43409289195414ad2dc961296d5ef71a7be245d
KGTurbo <117431635+KGTurbo@users.noreply.github.com>
2024-08-01 09:05:36 +0300

    Dissolve gibs

--- lua/zbase/npc_base_internal.lua
+++ lua/zbase/npc_base_internal.lua
@@ -3891,7 +3891,8 @@ function NPC:InternalCreateGib( model, data )
 
 
     -- Create
-    local Gib = ents.Create("base_gmodentity")
+    local entclass = !data.IsRagdoll && "base_gmodentity" or "prop_ragdoll"
+    local Gib = ents.Create(entclass)
     Gib:SetModel(model)
     Gib:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
     Gib.IsZBaseGib = true
@@ -3972,17 +3973,28 @@ function NPC:InternalCreateGib( model, data )
 
 
     -- Phys stuff
-    local phys = Gib:GetPhysicsObject()
+    local LastDMGInfo = self:LastDMGINFO()
+	if LastDMGInfo && LastDMGInfo:IsDamageType(DMG_DISSOLVE) && !data.DontDissolve then
+                    local dissolver = ents.Create("env_entity_dissolver")
+                    dissolver:SetPos(Gib:GetPos())
+                    dissolver:Spawn()
+                    dissolver:Activate()
+                    dissolver:SetKeyValue("magnitude",100)
+                    dissolver:SetKeyValue("dissolvetype", math.random(0, 2))
+                    Gib:SetName("z_dissolve_gib")
+                    dissolver:Fire("Dissolve","z_dissolve_gib")
+                    dissolver:Fire("Kill", "", 0.1)
+                end
+	for i = 0, Gib:GetPhysicsObjectCount() - 1 do
+local phys = Gib:GetPhysicsObjectNum(i)
     if IsValid(phys) then
 
         phys:Wake()
 
-        local LastDMGInfo = self:LastDMGINFO()
-
         if LastDMGInfo then
             local ForceDir = LastDMGInfo:GetDamageForce()/(math.Clamp(phys:GetMass(), 40, 10000))
             phys:SetVelocity( (ForceDir) + VectorRand()*(ForceDir:Length()*0.33) )
-        end
+        end end
 
     end
 
