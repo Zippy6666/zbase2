1337e6b83ce1bb40575a8a37321ceaff9c527d8b
Zippy6666 <simperzippy@gmail.com>
2024-12-21 14:01:26 +0100

    Revert "going to revert"
    This reverts commit decc131fb544c83ff1da4b96c6631f53552777e1.

--- lua/autorun/zbase.lua
+++ lua/autorun/zbase.lua
@@ -113,7 +113,6 @@ end
 
 game.AddParticles("particles/zbase/zbase_blood_impact.pcf")
 game.AddParticles("particles/zbase/hl2mmod_muzzleflashes_npc.pcf")
-game.AddParticles("particles/zbase/weapon_glock.pcf")
 
 game.AddParticles("particles/striderbuster.pcf")
 game.AddParticles("particles/mortarsynth_fx.pcf")
@@ -127,6 +126,7 @@ PrecacheParticleSystem("striderbuster_break_shell")
 PrecacheParticleSystem("hl2mmod_muzzleflash_npc_ar2")
 PrecacheParticleSystem("hl2mmod_muzzleflash_npc_pistol")
 PrecacheParticleSystem("hl2mmod_muzzleflash_npc_shotgun")
+
 
 --[[
 ======================================================================================================================================================
