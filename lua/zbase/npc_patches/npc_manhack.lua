2dbba52e10b81c27b486b2398cc6f4e8a2f01fcd
Zippy6666 <simperzippy@gmail.com>
2024-12-24 21:07:47 +0100

    manhacks use air nodes

---
+++ lua/zbase/npc_patches/npc_manhack.lua
@@ -0,0 +1,8 @@
+local my_cls = ZBasePatchNPCClass(debug.getinfo(1,'S'))
+local SF_MANHACK_USE_AIR_NODES = 262144
+
+ZBasePatchTable[my_cls] = function( NPC )
+    function NPC:Patch_PreSpawn()
+        self:CONV_AddSpawnFlags(SF_MANHACK_USE_AIR_NODES)
+    end
+end
