b888aebceb76269b8c76e16eb17d7bae56242798
Zippy6666 <simperzippy@gmail.com>
2024-12-25 23:37:40 +0100

    mark debugger as wip

--- lua/weapons/gmod_tool/stools/zbase_debugger.lua
+++ lua/weapons/gmod_tool/stools/zbase_debugger.lua
@@ -1,44 +1,44 @@
--- TOOL.AddToMenu = true
--- TOOL.Category = "ZBASE"
+TOOL.AddToMenu = true
+TOOL.Category = "ZBASE"
 
--- local toolname = "ZBASE: Debugger"
--- TOOL.Name = toolname
--- TOOL.Description = "Show information about NPCs."
-
-
--- local help = "Left-click: Debug the NPC at your crosshair."
--- if CLIENT then
---     language.Add("tool.zbase_debugger.name", TOOL.Name)
---     language.Add("tool.zbase_debugger.desc", TOOL.Description)
---     language.Add("tool.zbase_debugger.0", help)
--- end
+local toolname = "ZBASE: Debugger"
+TOOL.Name = toolname
+TOOL.Description = "Show information about NPCs. WIP"
 
 
--- function TOOL:LeftClick( trace )
---     local ent = trace.Entity
--- end
+local help = "Left-click: Debug the NPC at your crosshair."
+if CLIENT then
+    language.Add("tool.zbase_debugger.name", TOOL.Name)
+    language.Add("tool.zbase_debugger.desc", TOOL.Description)
+    language.Add("tool.zbase_debugger.0", help)
+end
 
 
--- function TOOL:RightClick( trace )
--- end
+function TOOL:LeftClick( trace )
+    local ent = trace.Entity
+end
 
 
--- function TOOL:Reload( trace )
--- end
+function TOOL:RightClick( trace )
+end
 
 
--- if CLIENT then
+function TOOL:Reload( trace )
+end
 
---     local function CreateEntry( panel, title, text )
---         local entry = panel:TextEntry(title)
---         entry:SetText(text)
---         entry:SetEditable(false)
---     end
 
---     function TOOL.BuildCPanel(panel)
---         panel:Help(help)
---         CreateEntry(panel, "Name", "")
---     end
+if CLIENT then
 
--- end
+    local function CreateEntry( panel, title, text )
+        local entry = panel:TextEntry(title)
+        entry:SetText(text)
+        entry:SetEditable(false)
+    end
+
+    function TOOL.BuildCPanel(panel)
+        panel:Help(help)
+        CreateEntry(panel, "Name", "")
+    end
+
+end
 
