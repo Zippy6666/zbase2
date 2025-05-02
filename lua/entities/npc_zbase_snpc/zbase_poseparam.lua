b2d641f34bd63faf5140bb9c236a9ec5f732f873
Zippy6666 <simperzippy@gmail.com>
2025-05-02 16:21:53 +0200

    fixes for poseparams

--- lua/entities/npc_zbase_snpc/zbase_poseparam.lua
+++ lua/entities/npc_zbase_snpc/zbase_poseparam.lua
@@ -4,7 +4,7 @@ function ENT:PoseParamThink()
     -- Looking pose parameter logic
     
     local center        = self:WorldSpaceCenter()
-    local idealLookAng  = self:GetLocalAngles()
+    local idealLookAng  = self:GetAngles()
     local ene           = self:GetEnemy()
 
     if IsValid(ene) then
@@ -13,14 +13,18 @@ function ENT:PoseParamThink()
         if IsValid(ene) then
             local eneLookPos = ene:GetPos() + Vector(0, 0, ene:OBBMaxs().z*0.6)
             local lookVec = eneLookPos - center
-            idealLookAng = self:WorldToLocalAngles(lookVec:Angle())
+            idealLookAng = lookVec:Angle()
         end
     end
 
+    debugoverlay.Line(center, center + idealLookAng:Forward() * 100, 0.1, Color(255, 0, 0), true)
+
+
+    idealLookAng = self:WorldToLocalAngles(idealLookAng)
     local idealYaw          = idealLookAng.yaw
     local idealPitch        = idealLookAng.pitch
-    self.CurPoseParamYaw    = Lerp(0.2, self.CurPoseParamPitch or idealYaw, idealYaw)
-    self.CurPoseParamPitch  = Lerp(0.2, self.CurPoseParamPitch or idealPitch, idealPitch)
+    self.CurPoseParamYaw    = Lerp(0.5, self.CurPoseParamPitch or idealYaw, idealYaw)
+    self.CurPoseParamPitch  = Lerp(0.5, self.CurPoseParamPitch or idealPitch, idealPitch)
 
     for _, v in ipairs(self.LookPoseParamNames.Yaw) do
         self:SetPoseParameter(v, self.CurPoseParamYaw)
