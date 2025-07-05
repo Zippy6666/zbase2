40305fe8e4301d4355583b1d5304ce88ace5a0cf
Zippy6666 <simperzippy@gmail.com>
2025-07-05 18:16:52 +0200

    fix

--- lua/zbase/npc_patches/npc_helicopter.lua
+++ lua/zbase/npc_patches/npc_helicopter.lua
@@ -1,9 +1,13 @@
 local my_cls = ZBasePatchNPCClass(debug.getinfo(1,'S'))
 
 ZBasePatchTable[my_cls] = function( NPC )
-    -- Prevents default gibs from spawning when the helicopter dies
     function NPC:Patch_PreDeath( dmg )
-        self:SetHealth(math.huge)
+        -- Must be called to get death notice + custom death run etc..
         hook.Run("OnNPCKilled", self, dmg:GetAttacker(), dmg:GetInflictor())
+
+        -- Prevents default gibs from spawning when the helicopter dies
+        -- Yes, both are needed
+        self:SetHealth(math.huge)
+        self:Remove()
     end
 end
