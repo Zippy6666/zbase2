1d16fc91b11ed97205e1261a058253b35d59668e
Zippy6666 <simperzippy@gmail.com>
2026-04-30 18:08:40 +0200

    RUBAT FIX

--- lua/zbase/npc_base/sh_classname.lua
+++ lua/zbase/npc_base/sh_classname.lua
@@ -1,25 +1,20 @@
--- This is an experimental component
+local NPC = ZBaseNPCs["npc_zbase"]
+local ENT = FindMetaTable("Entity")
 
-local NPC           = ZBaseNPCs["npc_zbase"]
+ZBASE_SkipClassNameWrapper = false
+
+-- A wrapper that returns the CustomClass instead of the class if it has one
+ENT.GetClass = conv.wrapFunc("ZBASE_ClassOverride", ENT.GetClass, function( self )
+    if self:GetNWBool("IsZBaseNPC", false)==true && self:GetNWString("ZBASE_CustomClass", "")!="" && !ZBASE_SkipClassNameWrapper then
+        return self:GetNWString("ZBASE_CustomClass", nil)
+    end
+end)
 
 if SERVER then
     function NPC:ApplyCustomClassName(clsname)
         if ZBCVAR.CustomClass:GetBool() then
             self:SetNWString("ZBASE_CustomClass", clsname)
-            self:SetKeyValue("classname", clsname)
+            -- self:SetKeyValue("classname", clsname) -- this doesn't work any more because rubat rubatted
         end
     end
-end
-
-if CLIENT then
-    local ENT           = FindMetaTable("Entity")
-
-    ZBASE_SkipClassNameWrapper = false
-
-    -- A wrapper that returns the CustomClass instead of the class if it has one
-    ENT.GetClass = conv.wrapFunc("ZBASE_ClassOverrideCL", ENT.GetClass, function( self )
-        if self:GetNWBool("IsZBaseNPC", false)==true && self:GetNWString("ZBASE_CustomClass", "")!="" && !ZBASE_SkipClassNameWrapper then
-            return self:GetNWString("ZBASE_CustomClass", nil)
-        end
-    end)
 end
