73752c4e1f5e270c339017c0f390541b396ab31f
Zippy6666 <simperzippy@gmail.com>
2026-05-23 23:02:03 +0200

    remove ass123

--- lua/zbase/sv_wepacc.lua
+++ lua/zbase/sv_wepacc.lua
@@ -23,8 +23,6 @@ hook.Add("EntityFireBullets", "ZBASE_WeaponAccuracy", function(ent, data)
     
     local wep = (isfunction(ent.GetActiveWeapon) && ent:GetActiveWeapon()) || NULL
 
-    print(wep, ent)
-
     if ent.IsZBaseWeapon || ent.IsZBaseNPC || wep.IsZBaseWeapon || wep.IsZBaseNPC then
         data.Spread.x = data.Spread.x/ZBCVAR.WepAccuracyMult:GetFloat()
         data.Spread.y = data.Spread.y/ZBCVAR.WepAccuracyMult:GetFloat()
@@ -32,8 +30,6 @@ hook.Add("EntityFireBullets", "ZBASE_WeaponAccuracy", function(ent, data)
         BulletHookRanAccuracyCode = true
         hook.Run("EntityFireBullets", ent, data)
         BulletHookRanAccuracyCode = false
-
-        print("ass123")
 
         return true
     end
